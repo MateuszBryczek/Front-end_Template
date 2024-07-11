c463e2fedbd5ffb9af456776f1ba2c3e9766ee89
Mateusz Bryczek <bryczek.mateusz02@gmail.com>
2024-07-11 18:14:53 +0200

    created Section and SectionHeader

---
+++ src/common/Section/index.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Section = styled.section`
+    box-shadow: 0px 5px 24px 0px ;
+    margin-left: 10px;
+    margin-right: 10px;
+`;
--- src/features/CssPage/styled.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const SectionHeader = styled.h2`
-
-`;
-
-export const Section = styled.section`
-
-`;
