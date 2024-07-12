210954315abf391ee7959e4e16697bccaf6f148a
Mateusz Bryczek <bryczek.mateusz02@gmail.com>
2024-07-12 19:12:14 +0200

    Section rebuilded, added SectionWrapper

--- src/common/Section/index.js
+++ src/common/Section/index.js
@@ -1,7 +1,22 @@
 import styled from "styled-components";
 
+export const Wrapper = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 8fr 1fr;
+    grid-gap: 5px;
+`;
+
+export const SectionHeader = styled.h2`
+    text-align: center;
+    
+`;
+
 export const Section = styled.section`
     box-shadow: 0px 5px 24px 0px ;
     margin-left: 10px;
     margin-right: 10px;
+`;
+
+export const SectionWrapper = styled.div`
+    grid-column: 2/3;
 `;
