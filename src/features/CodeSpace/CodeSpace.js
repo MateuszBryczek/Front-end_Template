59e139d23bc97c59fa2f805d8cf8526d2fd62b72
Mateusz Bryczek <bryczek.mateusz02@gmail.com>
2024-07-28 16:46:30 +0200

    changed gropups as categorys to colors

--- src/features/CodeSpace/CodeSpace.js
+++ src/features/CodeSpace/CodeSpace.js
@@ -5,26 +5,28 @@ import { StyledCodeSpace } from './styled'
 const CodeSpace = ({ children }) => {
     const theme = useTheme();
 
-const keywordGroups = {
-  variables: ['var', 'let', 'const', 'script'],
-  functions: ['function', 'return', ],
-  conditionals: ['if', 'else', 'for', 'while'],
-  symbols: [],
-  strings: ['".*?"', "'.*?'"],
-};
+  const keywordGroups = {
+    Blue: ['var', 'let', 'const', 'script', 'head', 'body', 'html', 'h1', 'h2', 'h3', '\\bp\\b', 'button', 'title'],
+    LightBlue: ['!DOCTYPE html', 'id', 'type', 'onclick', 'document', 'src'],
+    Purple: ['function', 'return', 'if', 'else', 'for', 'while', '{', '}'],
+    Green: [],
+    Orange: ['".*?"', "'.*?'"],
+    Grey: ['<', '>', '/'],
+  };
 
-const buildRegex = () => {
-  const groups = Object.values(keywordGroups).flat();
-  return new RegExp(`(${groups.join('|')})`, 'g');
-};
+  const buildRegex = () => {
+    const groups = Object.values(keywordGroups).flat();
+    return new RegExp(`(${groups.join('|')})`, 'g');
+  };
 
   const colors = {
-    variables: theme.CodeSpace.variables,
-    functions: theme.CodeSpace.functions,
-    conditionals: theme.CodeSpace.conditionals,
-    symbols: theme.CodeSpace.symbols,
-    strings: theme.CodeSpace.strings,
     default: theme.CodeSpace.default,
+    Blue: theme.CodeSpace.Blue,
+    LightBlue: theme.CodeSpace.LightBlue,
+    Purple: theme.CodeSpace.Purple,
+    Green: theme.CodeSpace.Green,
+    Orange: theme.CodeSpace.Orange,
+    Grey: theme.CodeSpace.Grey,
   };
 
   const splitText = (text) => {
