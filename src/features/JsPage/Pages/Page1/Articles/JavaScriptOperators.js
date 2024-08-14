8fff01e1066915f6226f8d51c2eddee6e042fd39
Mateusz Bryczek <bryczek.mateusz02@gmail.com>
2024-08-14 19:08:29 +0200

    added JS Comments and JS Operators for /JavaScript/JavaScriptBasics

---
+++ src/features/JsPage/Pages/Page1/Articles/JavaScriptOperators.js
@@ -0,0 +1,18 @@
+import { 
+    ArticleHeader,
+    SectionHeader,
+    Article, 
+    Section,
+    Highlighted,
+    Hint,
+} from "../../../../../common/Section";
+import CodeSpace from "../../../../CodeSpace/CodeSpace";
+
+export const JavaScriptOperators = () => (
+<>
+    <ArticleHeader id="7">JavaScript Operators</ArticleHeader>
+        <Article>
+<SectionHeader></SectionHeader>
+        </Article>
+</>
+);
