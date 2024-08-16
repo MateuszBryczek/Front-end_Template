cb069f89072083dc312a861b9c22dfe9b5dbccf6
Mateusz Bryczek <bryczek.mateusz02@gmail.com>
2024-08-16 20:28:05 +0200

    added "JavaScriptOperators" content

--- src/features/JsPage/Pages/Page1/Articles/JavaScriptOperators.js
+++ src/features/JsPage/Pages/Page1/Articles/JavaScriptOperators.js
@@ -6,13 +6,83 @@ import {
     Highlighted,
     Hint,
 } from "../../../../../common/Section";
+import Table from "../../../../../common/Tabele";
 import CodeSpace from "../../../../CodeSpace/CodeSpace";
+
+const arithmeticColumns = ['Operator', 'Description', 'Example'];
+const arithmeticData = [
+    { Operator: '+', Description: 'Addition', Example: 'x + y' },
+    { Operator: '-', Description: 'Subtraction', Example: 'x - y' },
+    { Operator: '*', Description: 'Multiplication', Example: 'x * y' },
+    { Operator: '**', Description: 'Exponentiation', Example: 'x ** y' },
+    { Operator: '/', Description: 'Division', Example: 'x / y' },
+    { Operator: '%', Description: 'Modulus', Example: 'x % y' },
+    { Operator: '++', Description: 'Increment', Example: 'x ++' },
+    { Operator: '--', Description: 'Decrement', Example: 'x --' },
+];
+const assignmentColumns = ['Operator', 'Same As', 'Example'];
+const assignmentData = [
+    { Operator: '=', 'Same As': 'x = y', Example: 'x = y' },
+    { Operator: '+=', 'Same As': 'x = x + y', Example: 'x =+ y' },
+    { Operator: '-=', 'Same As': 'x = x - y', Example: 'x -= y' },
+    { Operator: '*=', 'Same As': 'x = x * y', Example: 'x *= y' },
+    { Operator: '/=', 'Same As': 'x = x / y', Example: 'x /= y' },
+    { Operator: '%=', 'Same As': 'x = x % y', Example: 'x %= y' },
+    { Operator: '**=', 'Same As': 'x = x ** y', Example: 'x **= y' },
+];
+const shiftAssignmentColumns = ['Operator', 'Same As', 'Example'];
+const shiftAssignmentData = [
+    { Operator: '<<=', 'Same As': 'x = x << y', Example: 'x <<= y' },
+    { Operator: '>>=', 'Same As': 'x = x >> y', Example: 'x >>= y' },
+    { Operator: '>>>=', 'Same As': 'x = x >>> y', Example: 'x >>>= y' },
+];
+const bitwiseAssignmentColumns = ['Operator', 'Same As', 'Example'];
+const bitwiseAssignmentData = [
+    { Operator: '&=', 'Same As': 'x = x & y', Example: 'x &= y' },
+    { Operator: '^=', 'Same As': 'x = x ^ y', Example: 'x ^= y' },
+    { Operator: '|=', 'Same As': 'x = x | y', Example: 'x |= y' },
+];
+const logicalAssignmentColumns = ['Operator', 'Same As', 'Example'];
+const logicalAssignmentData = [
+    { Operator: '&&=', 'Same As': 'x = x && (x = y)', Example: 'x &&= y' },
+    { Operator: '||=', 'Same As': 'x = x || (x = y)', Example: 'x ||= y' },
+    { Operator: '??=', 'Same As': 'x = x ?? (x = y)', Example: 'x ??= y' },
+];
 
 export const JavaScriptOperators = () => (
 <>
     <ArticleHeader id="7">JavaScript Operators</ArticleHeader>
         <Article>
-<SectionHeader></SectionHeader>
+<SectionHeader>JavaScript Arithmetic Operators</SectionHeader>
+<Section>
+<b>Arithmetic</b> operators perform arithmetic on numbers (literals or variables).
+<Table columns={arithmeticColumns} data={arithmeticData}/>
+The <b>exponentiation</b> operator <Highlighted>**</Highlighted> raises the first operand to the power of the second operand. Produces the same result as <Highlighted>Math.pow(x,y)</Highlighted>
+<Hint>
+Operator precedence describes the order in which operations are performed in an <b>arithmetic expression</b>.
+</Hint>
+</Section>
+
+<SectionHeader>JavaScript Assignment Operators</SectionHeader>
+<Section>
+Assignment operators assign values to JavaScript variables.
+<Table columns={assignmentColumns} data={assignmentData}/>
+</Section>
+
+<SectionHeader>JavaScript Shift Assignment Operators</SectionHeader>
+<Section>
+<Table columns={shiftAssignmentColumns} data={shiftAssignmentData}/>
+</Section>
+
+<SectionHeader>JavaScript Bitwise Assignment Operators</SectionHeader>
+<Section>
+<Table columns={bitwiseAssignmentColumns} data={bitwiseAssignmentData}/>
+</Section>
+
+<SectionHeader>JavaScript Logical Assignment Operators</SectionHeader>
+<Section>
+<Table columns={logicalAssignmentColumns} data={logicalAssignmentData}/>
+</Section>
         </Article>
 </>
 );
