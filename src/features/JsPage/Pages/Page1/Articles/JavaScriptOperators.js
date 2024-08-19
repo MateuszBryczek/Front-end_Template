3d3ca79891997d9d4dfd811776033ee7f3817605
Mateusz Bryczek <bryczek.mateusz02@gmail.com>
2024-08-19 21:16:10 +0200

    added more content for "JavaScriptOperators"

--- src/features/JsPage/Pages/Page1/Articles/JavaScriptOperators.js
+++ src/features/JsPage/Pages/Page1/Articles/JavaScriptOperators.js
@@ -48,6 +48,30 @@ const logicalAssignmentData = [
     { Operator: '||=', 'Same As': 'x = x || (x = y)', Example: 'x ||= y' },
     { Operator: '??=', 'Same As': 'x = x ?? (x = y)', Example: 'x ??= y' },
 ];
+const comparisonColumns = ['Operator', 'Description', 'Example'];
+const comparisonData = [
+    { Operator: '==', Description: 'Equality', Example: 'x == y' },
+    { Operator: '!=', Description: 'Inequality', Example: 'x != y' },
+    { Operator: '===', Description: 'Identity', Example: 'x === y' },
+    { Operator: '!==', Description: 'Non-identity', Example: 'x !== y' },
+    { Operator: '>', Description: 'Greater tnan operator', Example: 'x > y' },
+    { Operator: '>=', Description: 'Greater tnan or equal', Example: 'x >= y' },
+    { Operator: '<', Description: 'Less than', Example: 'x < y' },
+    { Operator: '<=', Description: 'Less than or equal', Example: 'x <= y' },
+];
+const logicalColumns = ['Name', 'Operator', 'Usage'];
+const logicalData = [
+    { Name: 'Logical AND', Operator: '&&', Usage: 'return true if both are true' },
+    { Name: 'Logical OR', Operator: '||', Usage: 'return true if either is true' },
+    { Name: 'Logical NOT', Operator: '!', Usage: 'return true if is not true' },
+];
+const bitwiseColumns = ['Name', 'Operator', 'Usage'];
+const bitwiseData = [
+    { Name: 'AND', Operator: '&', Usage: '' },
+    { Name: 'OR', Operator: '|', Usage: '' },
+    { Name: 'XOR', Operator: '^', Usage: '' },
+    { Name: 'NOT', Operator: '~', Usage: '' },
+];
 
 export const JavaScriptOperators = () => (
 <>
@@ -82,6 +106,24 @@ Assignment operators assign values to JavaScript variables.
 <SectionHeader>JavaScript Logical Assignment Operators</SectionHeader>
 <Section>
 <Table columns={logicalAssignmentColumns} data={logicalAssignmentData}/>
+</Section>
+
+<SectionHeader>JavaScript Comparison Operators</SectionHeader>
+<Section>
+A <b>comparison operator</b> used to compare strings, numbers, Booleans, or object values. Returns a <b>Boolean</b> ( true or false).
+<Table columns={comparisonColumns} data={comparisonData}/>
+</Section>
+
+<SectionHeader>JavaScript Logical Operators</SectionHeader>
+<Section>
+<b>Logical operators</b> are commonly used with <b>Booleans</b>. Returns a <b>Boolean</b> value ( true or false).
+<Table columns={logicalColumns} data={logicalData}/>
+</Section>
+
+<SectionHeader>JavaScript Bitwise Operators</SectionHeader>
+<Section>
+<b>Logical operators</b> are commonly used with <b>Booleans</b>. Returns a <b>Boolean</b> value ( true or false).
+<Table columns={bitwiseColumns} data={bitwiseData}/>
 </Section>
         </Article>
 </>
