052ebdb1fff2bb68e6882d7cd28843b6cca758ad
Mateusz Bryczek <bryczek.mateusz02@gmail.com>
2024-08-26 18:14:17 +0200

    added content for "JavaScriptEvents"

--- src/features/JsPage/Pages/Page1/Articles/JavaScriptEvents.js
+++ src/features/JsPage/Pages/Page1/Articles/JavaScriptEvents.js
@@ -17,10 +17,35 @@ export const JavaScriptEvents = () => (
 <b>Events</b> are <b>actions</b> that happen in the system you are programing.<br/>
 The system fires a signal when events happen, so we can do sometking when they do.
 <p/>
-For example, we can perform a task when a <b>a button is clicked</b>.
+For example, we can perform a task when <b>a button is clicked</b>.
 <CodeSpace>
 {`
-const 
+const event = document.getElementById(event);
+
+event.onclick = function() {
+  alert("The button was clicked");
+};
+`}
+</CodeSpace>
+<Hint>The block of code that executes when an event fires is called <b>event handler</b> and <b>event listener</b>.</Hint>
+You can also write event handlers inside an HTML element.
+<CodeSpace>
+{`
+<button onclick='alert'("The button was clicked")>Click me</button>
+`}
+</CodeSpace>
+</Section>
+
+<SectionHeader>The addEventListener()</SectionHeader> Method
+<Section>
+The <Highlighted>addEventListener</Highlighted> method sets up a <b>function</b> that will be called when an event happens.
+<CodeSpace>
+{`
+const event = document.getElementById("event");
+
+event.addEventListener("click", function() {
+  alert("The button was clicked");
+});
 `}
 </CodeSpace>
 </Section>
