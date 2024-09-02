b2cdd17c97143213671a388d8a53d15d488262d3
Mateusz Bryczek <bryczek.mateusz02@gmail.com>
2024-09-02 18:22:38 +0200

    added more content

--- src/features/JsPage/Pages/Page2/Articles/JavaScriptStrings.js
+++ src/features/JsPage/Pages/Page2/Articles/JavaScriptStrings.js
@@ -7,6 +7,30 @@ import {
     Hint,
 } from "../../../../../common/Section";
 import CodeSpace from "../../../../CodeSpace/CodeSpace";
+import Table from "../../../../../common/Tabele";
+
+const stringColumns = ['Name', 'Usage', 'Return'];
+const stringData = [
+    {Name: "String length", Usage: "text.length;", Return: "The length of a string"},
+    {Name: "String charAt()", Usage: "text.charAt(x);", Return: "The character at a specified index"},
+    {Name: "String charCodeAt()", Usage: "text.charCodeAt(x);", Return: "The code of the character at a specified index"},
+    {Name: "String at()", Usage: "text.at(x);", Return: "The x letter of index"},
+    {Name: "String []", Usage: "text[x]", Return: "The x letter of index"},
+    {Name: "String slice()", Usage: "text.slice(x, y);", Return: "A part of a string from x to y"},
+    {Name: "String substring()", Usage: "text.substring(x, y);", Return: "A part of a string from x to y"},
+    {Name: "String substr()", Usage: "text.length", Return: "A part of a string from x to y"},
+    {Name: "String toUpperCase()", Usage: "text.length", Return: "the length of a string"},
+    {Name: "String toLowerCase()", Usage: "text.length", Return: "the length of a string"},
+    {Name: "String trim()", Usage: "text.length", Return: "the length of a string"},
+    {Name: "String trimStart()", Usage: "text.length", Return: "the length of a string"},
+    {Name: "String trimEnd()", Usage: "text.length", Return: "the length of a string"},
+    {Name: "String padStart()", Usage: "text.length", Return: "the length of a string"},
+    {Name: "String padEnd()", Usage: "text.length", Return: "the length of a string"},
+    {Name: "String repeat()", Usage: "text.length", Return: "the length of a string"},
+    {Name: "String replace()", Usage: "text.length", Return: "the length of a string"},
+    {Name: "String replaceAll()", Usage: "text.length", Return: "the length of a string"},
+    {Name: "String split()", Usage: "text.length", Return: "the length of a string"},
+];
 
 export const JavaScriptStrings = () => (
 <>
@@ -51,6 +75,11 @@ const str3 = "The character \\\\ is called backslash.";
 <Hint>
 <b>Escaping characters</b> will avoid many common errors in writing strings.
 </Hint>
+</Section>
+
+<SectionHeader>JavaScript String Methods</SectionHeader>
+<Section>
+<Table columns={stringColumns} data={stringData} />
 </Section>
     </Article>
 </>
