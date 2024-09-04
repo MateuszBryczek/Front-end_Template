6596f8b94bbb9954f1e0c14f1399fb373f952c96
Mateusz Bryczek <bryczek.mateusz02@gmail.com>
2024-09-04 16:55:01 +0200

    added more content for "JavaScriptStrings"

--- src/features/JsPage/Pages/Page2/Articles/JavaScriptStrings.js
+++ src/features/JsPage/Pages/Page2/Articles/JavaScriptStrings.js
@@ -9,27 +9,28 @@ import {
 import CodeSpace from "../../../../CodeSpace/CodeSpace";
 import Table from "../../../../../common/Tabele";
 
-const stringColumns = ['Name', 'Usage', 'Return'];
+const stringColumns = ['Name', 'Usage', 'Output'];
 const stringData = [
-    {Name: "String length", Usage: "text.length;", Return: "The length of a string"},
-    {Name: "String charAt()", Usage: "text.charAt(x);", Return: "The character at a specified index"},
-    {Name: "String charCodeAt()", Usage: "text.charCodeAt(x);", Return: "The code of the character at a specified index"},
-    {Name: "String at()", Usage: "text.at(x);", Return: "The x letter of index"},
-    {Name: "String []", Usage: "text[x]", Return: "The x letter of index"},
-    {Name: "String slice()", Usage: "text.slice(x, y);", Return: "A part of a string from x to y"},
-    {Name: "String substring()", Usage: "text.substring(x, y);", Return: "A part of a string from x to y"},
-    {Name: "String substr()", Usage: "text.length", Return: "A part of a string from x to y"},
-    {Name: "String toUpperCase()", Usage: "text.length", Return: "the length of a string"},
-    {Name: "String toLowerCase()", Usage: "text.length", Return: "the length of a string"},
-    {Name: "String trim()", Usage: "text.length", Return: "the length of a string"},
-    {Name: "String trimStart()", Usage: "text.length", Return: "the length of a string"},
-    {Name: "String trimEnd()", Usage: "text.length", Return: "the length of a string"},
-    {Name: "String padStart()", Usage: "text.length", Return: "the length of a string"},
-    {Name: "String padEnd()", Usage: "text.length", Return: "the length of a string"},
-    {Name: "String repeat()", Usage: "text.length", Return: "the length of a string"},
-    {Name: "String replace()", Usage: "text.length", Return: "the length of a string"},
-    {Name: "String replaceAll()", Usage: "text.length", Return: "the length of a string"},
-    {Name: "String split()", Usage: "text.length", Return: "the length of a string"},
+    {Name: "String length", Usage: "text.length;", Output: "The length of a string"},
+    {Name: "String charAt()", Usage: "text.charAt(x);", Output: "The character at a specified index"},
+    {Name: "String charCodeAt()", Usage: "text.charCodeAt(x);", Output: "The code of the character at a specified index"},
+    {Name: "String at()", Usage: "text.at(x);", Output: "The x letter of index"},
+    {Name: "String []", Usage: "text[x]", Output: "The x letter of index"},
+    {Name: "String slice()", Usage: "text.slice(x, y);", Output: "A part of a string from x to y"},
+    {Name: "String substring()", Usage: "text.substring(x, y);", Output: "A part of a string from x to y"},
+    {Name: "String substr()", Usage: "text.substr(x, y);", Output: "A part of a string from x to y"},
+    {Name: "String toUpperCase()", Usage: "text.toUpperCase();", Output: "A string converted to upper case"},
+    {Name: "String toLowerCase()", Usage: "text.toLowerCase();", Output: "A string converted to upper case"},
+    {Name: "String concat()", Usage: "text.concat(text1, text2);", Output: "Joins multiple strings"},
+    {Name: "String trim()", Usage: "text.trim();", Output: "Removes whitespace from both sides of a string"},
+    {Name: "String trimStart()", Usage: "text.trimStart();", Output: "Removes whitespace from the start of a string"},
+    {Name: "String trimEnd()", Usage: "text.trimEnd();", Output: "Removes whitespace from the end of a string"},
+    {Name: "String padStart()", Usage: "text.padStart(x, y)", Output: "Pad a string with y from the start, until it reaches the length x"},
+    {Name: "String padEnd()", Usage: "text.padEnd(x, y)", Output: "Pad a string with y from the end, until it reaches the length x"},
+    {Name: "String repeat()", Usage: "text.repeat(x)", Output: "A string with a number of x copies of a string"},
+    {Name: "String replace()", Usage: "text.replace(text1, text2)", Output: "Replaces a specified value with another value in a string"},
+    {Name: "String replaceAll()", Usage: "text.ReplaceAll(text1, text2)", Output: "Replaces all specified values with another values in a string"},
+    {Name: "String split()", Usage: 'text.split(" ")', Output: "Splits a string into an array of substrings"},
 ];
 
 export const JavaScriptStrings = () => (
