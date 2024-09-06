411abcc43fb273ca97c00b61cbfdcb72a565c611
Mateusz Bryczek <bryczek.mateusz02@gmail.com>
2024-09-06 16:53:55 +0200

    added content for JavaScriptNumber

--- src/features/JsPage/Pages/Page2/Articles/JavaScriptNumber.js
+++ src/features/JsPage/Pages/Page2/Articles/JavaScriptNumber.js
@@ -9,13 +9,22 @@ import {
 import CodeSpace from "../../../../CodeSpace/CodeSpace";
 import Table from "../../../../../common/Tabele";
 
+const javaScriptNumberColumns = ["Name", "Usage", "Output"];
+const javaScriptNumberData = [
+    {Name: "Number toString()", Usage: "x.toString()", Output: "Returns x as a string"},
+    {Name: "Namber toExponential(y)", Usage: "x.toExponential(y)", Output: "Returns a string with x rounded with y number of decimal point"},
+    {Name: "Number toFixed()", Usage: "x.toFixed(y)", Output: "Returns a string with x with y number of decimal point"},
+    {Name: "Number toPrecision()", Usage: "x.toPrecision(y)", Output: "Returns a string with x with y number length"},
+    {Name: "Number valueOf()", Usage: "x.valueOf()", Output: "Returns a x as a number"},
+];
+
 export const JavaScriptNumber = () => (
 <>
     <ArticleHeader id="2">JavaScript Number</ArticleHeader>
     <Article>
-<SectionHeader></SectionHeader>
+<SectionHeader>JavaScript Number Methods</SectionHeader>
 <Section>
-
+<Table columns={javaScriptNumberColumns} data={javaScriptNumberData}/>:
 </Section>
     </Article>
 </>
