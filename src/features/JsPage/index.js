ca0d253325b8639adad43a51972f4dba6978e810
Mateusz Bryczek <bryczek.mateusz02@gmail.com>
2024-11-21 13:39:18 +0100

    added Page1 as default page in JsPage

--- src/features/JsPage/index.js
+++ src/features/JsPage/index.js
@@ -3,7 +3,7 @@ import { Wrapper, } from "../../common/Section";
 import NavScrollbar from "../../common/Navigation/NavScrollbar";
 import useThemeColor from "../useThemeColor/useThemeColor";
 import { Outlet } from "react-router-dom";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import Page1 from "./Pages/Page1/JavaScriptBasics";
 import Page2 from "./Pages/Page2/JavaScriptMoreOnDataTypes";
 
@@ -16,6 +16,7 @@ const JsPage = () => {
         <NavScrollbar/>
         <Header $getThemeColor={ themeColor }>JavaScript</Header>
             <Routes>
+                <Route index element={<Navigate to="JavaScriptBasics" replace />} />
                 <Route path="JavaScriptBasics" element={<Page1 />} />
                 <Route path="JavaScriptMoreOnDataTypes" element={<Page2 />} />
             </Routes>
